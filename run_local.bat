5c263a72808cd8f2d912d1b13cbc23647a6e111c
Matias Matavel <matiasmatavel@gmail.com>
2026-07-31 14:10:37 +0200

    corrigindo cotações2

--- run_local.bat
+++ run_local.bat
@@ -7,12 +7,12 @@ echo ========================================
 
 echo.
 echo [INFO] A iniciar Flutter Windows apontando para Coolify VPS...
-echo [INFO] Backend: http://c70gq7ownuk9h97yezcz0h3t.162.35.186.240.sslip.io
+echo [INFO] Backend: http://f4olimmjw7g6tiqyjskitxjg.162.35.186.240.sslip.io
 echo.
 
 cd /d "C:\pdv-stech engenharia\frontend\pdv_stech"
 
-flutter run -d windows -v --no-pub --dart-define=API_BASE_URL=http://c70gq7ownuk9h97yezcz0h3t.162.35.186.240.sslip.io
+flutter run -d windows -v --no-pub --dart-define=API_BASE_URL=http://f4olimmjw7g6tiqyjskitxjg.162.35.186.240.sslip.io
 
 echo.
 echo ========================================
