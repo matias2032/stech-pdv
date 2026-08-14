9e0b532894d4c527f5e681322452bd8f47fec6b4
Matias Matavel <matiasmatavel@gmail.com>
2026-08-14 10:23:56 +0200

    xet

--- run_local.bat
+++ run_local.bat
@@ -1,3 +1,4 @@
+
 @echo off
 title STech PDV  Flutter Launcher
 
@@ -9,7 +10,7 @@ echo ========================================
 
 echo.
 echo [INFO] A iniciar Flutter Windows apontando para Coolify VPS...
-echo [INFO] Backend: http://api-pdv.162.35.186.240.sslip.io
+echo [INFO] Backend: 
 echo.
 
 cd /d "C:\pdv-stech engenharia\frontend\pdv_stech"
